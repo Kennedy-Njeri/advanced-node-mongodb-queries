5c1ba380db448265674d56b91f34edb232929147
Kennedy Njeri <mistakenz123@gmail.com>
2020-09-07 20:26:51 +0300

    create bootcamp post req

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const router = new express.Router()
+const Bootcamp = require('../models/Bootcamp')
 
 
+router.post('/api/v1/bootcamps', async (req, res) => {
+    //console.log(req.body)
+    try {
+        const bootcamp = await Bootcamp.create(req.body)
+        res.status(201).json({ success: true, data: bootcamp })
+    } catch (e) {
+        res.status(400).json({ success: false })
+    }
+
+})
 
 router.post('/api/v1/bootcamps', (req, res) => {
     res.status(200).json({ success: true, msg: 'Show all bootcamps'})
@@ -12,10 +23,6 @@ router.get('/api/v1/bootcamps/:id', (req, res) => {
     res.status(200).json({ success: true, msg: `Show all bootcamps ${req.params.id}`})
 })
 
-
-router.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Create new bootcamp'})
-})
 
 router.put('/api/v1/bootcamps/:id', (req, res) => {
     res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}`})
