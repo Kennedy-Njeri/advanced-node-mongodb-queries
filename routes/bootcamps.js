c90e58a3401ecdc1a278c085b52c318adcc8d64e
Kennedy Njeri <mistakenz123@gmail.com>
2020-09-07 20:55:52 +0300

    retrieve single bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,10 +5,15 @@ const Bootcamp = require('../models/Bootcamp')
 
 router.post('/api/v1/bootcamps', async (req, res) => {
     //console.log(req.body)
+
     try {
+
         const bootcamp = await Bootcamp.create(req.body)
+
         res.status(201).json({ success: true, data: bootcamp })
+
     } catch (e) {
+
         res.status(400).json({ success: false })
     }
 
@@ -17,17 +22,35 @@ router.post('/api/v1/bootcamps', async (req, res) => {
 router.get('/api/v1/bootcamps', async (req, res) => {
 
     try {
+
         const bootcamps = await Bootcamp.find()
+
         res.status(200).json({ success: true, data: bootcamps })
+
     } catch (e) {
+
         res.status(500).json({ success: false })
     }
 
 })
 
 
-router.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Show all bootcamps ${req.params.id}`})
+router.get('/api/v1/bootcamps/:id', async (req, res) => {
+
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id)
+
+        if (!bootcamp) {
+            return res.status(400).json({ success: false })
+        }
+
+        res.status(200).json({ success: true, data: bootcamp })
+
+    } catch (e) {
+
+        res.status(400).json({ success: false })
+    }
+
 })
 
 
